24d6a146f9d8afd171cce517711ad984f18a6747
ladycattv <ladyccattv@gmail.com>
2026-09-19 18:24:29 +0200

    readded fireworks and nullpointer check cause git hates them

--- src/main/java/gg/crystalized/lobby/parkour/Parkour.java
+++ src/main/java/gg/crystalized/lobby/parkour/Parkour.java
@@ -4,6 +4,7 @@ import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.protocol.entity.data.EntityData;
 import com.github.retrooper.packetevents.protocol.entity.data.EntityDataTypes;
 import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes;
+import com.github.retrooper.packetevents.protocol.player.User;
 import com.github.retrooper.packetevents.util.Vector3d;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerDestroyEntities;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata;
@@ -13,12 +14,11 @@ import gg.crystalized.lobby.Lobby_plugin;
 import gg.crystalized.lobby.Nametag;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.NamespacedKey;
+import org.bukkit.*;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -30,6 +30,7 @@ import static net.kyori.adventure.text.format.NamedTextColor.*;
 import static net.kyori.adventure.text.format.TextDecoration.ITALIC;
 import static org.bukkit.Material.COAL;
 import static org.bukkit.attribute.Attribute.*;
+import static org.bukkit.entity.EntityType.FIREWORK_ROCKET;
 import static org.bukkit.potion.PotionEffectType.JUMP_BOOST;
 
 public class Parkour {
@@ -72,7 +73,8 @@ public class Parkour {
                     Location loc = checkpoints[0];
                     WrapperPlayServerSpawnEntity entity = new WrapperPlayServerSpawnEntity(checkpointEntity, UUID.randomUUID(), EntityTypes.SULFUR_CUBE, new com.github.retrooper.packetevents.protocol.world.Location
                             (loc.getX(), loc.getY(), loc.getZ(), 0, 0), 0, 0, new Vector3d());
-                    PacketEvents.getAPI().getPlayerManager().getUser(p).sendPacket(entity);
+                    User user = PacketEvents.getAPI().getPlayerManager().getUser(p);
+                    if(user != null) user.sendPacket(entity);
                 }
             }
         }.runTaskTimerAsynchronously(Lobby_plugin.getInstance(), 3, 3);
@@ -148,7 +150,7 @@ class ParkourRun{
         if(lastCheckpoint == course.checkpoints.length-1){
             stop(true);
         }
-        //p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1f, 1f);
+        p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1f, 1f);
 
     }
 
@@ -158,7 +160,17 @@ class ParkourRun{
         InventoryManager.giveLobbyItems(p);
         timer.task.cancel();
         running.remove(this);
-        if(finished) ParkourDatabase.saveRun(this);
+        if(!finished) return;
+        ParkourDatabase.saveRun(this);
+
+        Firework firework = (Firework)p.getWorld().spawnEntity(p.getLocation(), FIREWORK_ROCKET);
+        FireworkEffect.Builder effect = FireworkEffect.builder();
+        effect.with(FireworkEffect.Type.STAR);
+        effect.withColor(hex2Rgb(course.color.asHexString()));
+        FireworkMeta meta = firework.getFireworkMeta();
+        meta.addEffect(effect.build());
+        firework.setFireworkMeta(meta);
+        firework.detonate();
     }
 
     public void showOrHideCheckpoint(){
@@ -175,6 +187,13 @@ class ParkourRun{
         List<EntityData<?>> data = List.of(new EntityData(0, EntityDataTypes.BYTE, ((Integer)0x40).byteValue()));
         WrapperPlayServerEntityMetadata metadata = new WrapperPlayServerEntityMetadata(course.checkpointEntity, data);
         PacketEvents.getAPI().getPlayerManager().getUser(p).sendPacket(metadata);
+    }
+
+    public static Color hex2Rgb(String colorStr) {
+        return Color.fromRGB(
+                Integer.valueOf( colorStr.substring( 1, 3 ), 16 ),
+                Integer.valueOf( colorStr.substring( 3, 5 ), 16 ),
+                Integer.valueOf( colorStr.substring( 5, 7 ), 16 ) );
     }
 }
 
