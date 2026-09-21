713880813c1454402990452aeab780b834ab5349
ladycattv <ladyccattv@gmail.com>
2026-09-21 19:22:51 +0200

    fixed translation for unqueue button

--- src/main/java/gg/crystalized/lobby/App.java
+++ src/main/java/gg/crystalized/lobby/App.java
@@ -74,7 +74,7 @@ public enum App {
     Home("ui/invisible", useCases.UI, Component.translatable("crystalized.shardcore.generic.home").color(WHITE).decoration(ITALIC, false), null),
     Requeue("ui/replay", useCases.Demand, Component.translatable("crystalized.game.generic.requeue").color(WHITE).decoration(ITALIC, false), 7),
     BackToHub("ui/leave", useCases.Demand, Component.translatable("crystalized.game.generic.to_lobby").color(WHITE).decoration(ITALIC, false), 8),
-    Unqueue("ui/leave", useCases.Demand, Component.translatable("crystalized.game.generic.unqueue").color(WHITE).decoration(ITALIC, false), 8),
+    Unqueue("ui/leave", useCases.Demand, Component.translatable("crystalized.game.generic.queue.unqueue").color(WHITE).decoration(ITALIC, false), 8),
     MsgSetting("ui/scn3/settings/msg", useCases.Set, new useCases[]{useCases.Settings}, Component.translatable("crystalized.shardcore.settings.direct_messages").color(WHITE).decoration(ITALIC, false), 30, "dms"),
     FriendRequestSetting("ui/scn3/settings/friend_requests", useCases.Set, new useCases[]{useCases.Settings}, Component.translatable("crystalized.shardcore.settings.friend_requests").color(WHITE).decoration(ITALIC, false), 39, "friends_requests"),
     PartyRequestSetting("ui/scn3/settings/party_requests", useCases.Set, new useCases[]{useCases.Settings}, Component.translatable("crystalized.shardcore.settings.party_invites").color(WHITE).decoration(ITALIC, false), 48, "party_requests"),
